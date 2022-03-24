29d2a920a029f5af6a607b2e38b9a80e18424c57
haingocpham19 <haingocpham@hotmail.com>
2022-03-24 18:18:31 +0700

    stuff

--- js/custom.js
+++ js/custom.js
@@ -34,15 +34,60 @@ $(document).ready(function(){
     })
 });
 
-document.addEventListener('scroll', function(e) {
-  lastKnownScrollPosition = window.scrollY;
 
-  if (!ticking) {
-    window.requestAnimationFrame(function() {
-      doSomething(lastKnownScrollPosition);
-      ticking = false;
-    });
 
-    ticking = true;
+//click ontop
+var mybutton = document.getElementById("ontop");
+window.onscroll = function() {
+  scrollFunction()
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
   }
-});
+}
+// Bind your button click, scroll direction and effect speed
+document.getElementById("ontop").onclick = function() {
+  scrollTo(0, 8000); // it will take 8 seconds to reach to top.
+
+}
+
+// Element to move, time in ms to animate
+function scrollTo(element, duration) {
+  var e = document.documentElement;
+  if (e.scrollTop === 0) {
+    var t = e.scrollTop;
+    ++e.scrollTop;
+    e = t + 1 === e.scrollTop-- ? e : document.body;
+  }
+  scrollToC(e, e.scrollTop, element, duration);
+}
+
+// Element to move, element or px from, element or px to, time in ms to animate
+function scrollToC(element, from, to, duration) {
+  if (duration <= 0) return;
+  if (typeof from === "object") from = from.offsetTop;
+  if (typeof to === "object") to = to.offsetTop;
+
+  scrollToX(element, from, to, 0, 1 / duration, 20, easeOutCuaic);
+}
+
+function scrollToX(element, xFrom, xTo, t01, speed, step, motion) {
+  if (t01 < 0 || t01 > 1 || speed <= 0) {
+    element.scrollTop = xTo;
+    return;
+  }
+  element.scrollTop = xFrom - (xFrom - xTo) * motion(t01);
+  t01 += speed * step;
+  setTimeout(function() {
+    scrollToX(element, xFrom, xTo, t01, speed, step, motion);
+  }, step);
+}
+
+function easeOutCuaic(t) {
+  t--;
+  return t * t * t + 1;
+}
